7d398553d465e82a7aa5face2a76761eda2f59b0
Łukasz Antończyk <antonczyklukasz@gmail.com>
2019-02-26 14:19:07 +0100

    Inject event store module into aggregate server (#3)

--- lib/event_sourcing/aggregates/aggregate.ex
+++ lib/event_sourcing/aggregates/aggregate.ex
@@ -5,8 +5,8 @@ defmodule EventSourcing.Aggregates.Aggregate do
 
   @registry EventSourcing.AggregateRegistry
 
-  def start_link({_mod, _uuid} = aggregate) do
-    GenServer.start_link(__MODULE__, aggregate, name: name(aggregate))
+  def start_link({{_mod, _uuid} = aggregate, opts}) do
+    GenServer.start_link(__MODULE__, {aggregate, opts}, name: name(aggregate))
   end
 
   def execute_command(aggregate_pid, command) when is_pid(aggregate_pid) do
@@ -17,28 +17,29 @@ defmodule EventSourcing.Aggregates.Aggregate do
     {:via, Registry, {@registry, aggregate}}
   end
 
-  def init({aggregate_mod, aggregate_uuid}) do
+  def init({{aggregate_mod, aggregate_uuid}, opts}) do
     aggregate = apply(aggregate_mod, :__struct__, [[uuid: aggregate_uuid]])
 
     state = %{
       aggregate_mod: aggregate_mod,
       aggregate_uuid: aggregate_uuid,
-      aggregate: aggregate
+      aggregate: aggregate,
+      store_mod: Keyword.get(opts, :store, AgentEventStore)
     }
 
     {:ok, state}
   end
 
   def handle_call({:execute_command, command}, _from, state) do
-    %{aggregate_mod: aggregate_mod, aggregate: aggregate} = state
+    %{aggregate_mod: aggregate_mod, aggregate: aggregate, store_mod: store} = state
     event = aggregate_mod.execute(aggregate, command)
     aggregate = aggregate_mod.apply(aggregate, event)
-    store_event(aggregate, event)
+    store_event(store, aggregate, event)
 
     {:reply, {event, aggregate}, %{state | aggregate: aggregate}}
   end
 
-  defp store_event(%{uuid: uuid} = _aggregate, event) do
-    AgentEventStore.put(uuid, event)
+  defp store_event(store, %{uuid: uuid} = _aggregate, event) do
+    store.put(uuid, event)
   end
 end
--- lib/event_sourcing/aggregates/aggregate_supervisor.ex
+++ lib/event_sourcing/aggregates/aggregate_supervisor.ex
@@ -7,8 +7,8 @@ defmodule EventSourcing.Aggregates.AggregateSupervisor do
     DynamicSupervisor.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
-  def start_aggregate({_mod, _uuid} = aggregate) do
-    child_spec = Aggregate.child_spec(aggregate)
+  def start_aggregate({_mod, _uuid} = aggregate, opts) do
+    child_spec = Aggregate.child_spec({aggregate, opts})
 
     DynamicSupervisor.start_child(__MODULE__, child_spec)
   end
