54123668fda6d107a41a96d249862d0ecd12dec8
Łukasz Antończyk <antonczyklukasz@gmail.com>
2019-03-26 17:24:32 +0100

    Refactor EctoEventStore tests

--- apps/event_sourcing/test/event_sourcing/event_store/ecto_event_store_test.exs
+++ apps/event_sourcing/test/event_sourcing/event_store/ecto_event_store_test.exs
@@ -1,50 +1,49 @@
 defmodule EventSourcing.EventStore.EctoEventStoreTest do
   use EventSourcing.DataCase
 
+  import EventSourcing.Factory
+
   alias EventSourcing.EventStore.EctoEventStore
-  alias EventSourcing.EventStore.StoredEvent
   alias EventSourcing.Counters.Events.Incremented
 
   setup do
     {:ok, _} = start_supervised(EctoEventStore)
     stream_id = Ecto.UUID.generate()
-    event = %Incremented{uuid: Ecto.UUID.generate(), counter_uuid: stream_id}
-    {:ok, stream_id: stream_id, event: event}
+    {:ok, stream_id: stream_id}
   end
 
   describe "put/2" do
+    setup %{stream_id: stream_id} do
+      event = %Incremented{uuid: Ecto.UUID.generate(), counter_uuid: stream_id}
+      {:ok, event: event}
+    end
+
     test "stores event", %{stream_id: stream_id, event: event} do
       assert EctoEventStore.get(stream_id) == []
 
       EctoEventStore.put(stream_id, event)
       assert EctoEventStore.get(stream_id) == [event]
     end
+  end
 
-    test "stores multiple events", %{stream_id: stream_id, event: event} do
-      assert EctoEventStore.get(stream_id) == []
+  describe "get/1" do
+    test "returns events in proper order", %{stream_id: stream_id} do
+      events =
+        for _ <- 1..5 do
+          insert(:stored_event, stream_id: stream_id)
+        end
 
-      event1 = %Incremented{event | uuid: Ecto.UUID.generate()}
-      event2 = %Incremented{event | uuid: Ecto.UUID.generate()}
-      EctoEventStore.put(stream_id, event1)
-      EctoEventStore.put(stream_id, event2)
-
-      assert EctoEventStore.get(stream_id) == [event1, event2]
+      assert EctoEventStore.get(stream_id) == Enum.map(events, &Map.get(&1, :event_data))
     end
   end
+
   describe "next_event_number/0" do
     test "returns 1 for first event" do
       assert EctoEventStore.next_event_number() == 1
     end
 
-    test "returns max(event_number) + 1", %{stream_id: stream_id, event: event} do
-      %StoredEvent{
-        event_id: event.uuid,
-        stream_id: stream_id,
-        event_name: "event",
-        event_data: event,
-        event_number: 42
-      }
-      |> Repo.insert!()
+    test "returns max(event_number) + 1", %{stream_id: stream_id} do
+      insert(:stored_event, stream_id: stream_id, event_number: 42)
 
       stop_supervised(EctoEventStore)
       start_supervised(EctoEventStore)
