066e3872618b9036bcba18926f2908546742aeac
Dawid Kuśmierek <kusmierekd@gmail.com>
2019-03-26 13:35:07 +0100

    Created tests for approve/reject time off request events for TeamLeader.

--- apps/absence/test/absence/absences/aggregates/team_leader_test.exs
+++ apps/absence/test/absence/absences/aggregates/team_leader_test.exs
@@ -43,6 +43,23 @@ defmodule Absence.Absences.Aggregates.TeamLeaderTest do
     end
   end
 
+  test "TimeoffRequestApproved event adds TimeoffRequest to approved timeoff requests", %{
+    employee: employee,
+    team_leader: team_leader,
+    timeoff_request: timeoff_request
+  } do
+    event =
+      build_event(:timeoff_request_approved,
+        team_leader_uuid: team_leader.uuid,
+        employee_uuid: employee.uuid,
+        timeoff_request: timeoff_request
+      )
+
+    actual_team_leader = TeamLeader.apply(team_leader, event)
+    assert %{approved_timeoff_requests: [timeoff_request]} = actual_team_leader
+    assert %{rejected_timeoff_requests: []} = actual_team_leader
+  end
+
   describe "rejecting timeoff requests" do
     test "RejectTimeoffRequest rejects time off for specified employee", %{
       employee: employee,
@@ -62,5 +79,22 @@ defmodule Absence.Absences.Aggregates.TeamLeaderTest do
                timeoff_request: timeoff_request
              }
     end
+  end
+
+  test "TimeoffRequestRejected event adds TimeoffRequest to rejected timeoff requests", %{
+    employee: employee,
+    team_leader: team_leader,
+    timeoff_request: timeoff_request
+  } do
+    event =
+      build_event(:timeoff_request_rejected,
+        team_leader_uuid: team_leader.uuid,
+        employee_uuid: employee.uuid,
+        timeoff_request: timeoff_request
+      )
+
+    actual_team_leader = TeamLeader.apply(team_leader, event)
+    assert %{approved_timeoff_requests: []} = actual_team_leader
+    assert %{rejected_timeoff_requests: [timeoff_request]} = actual_team_leader
   end
 end
