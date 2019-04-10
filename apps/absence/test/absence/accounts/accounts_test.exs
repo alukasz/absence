849e0fddf2050630402a7a2c2d8e110e54ab511a
Łukasz Antończyk <antonczyklukasz@gmail.com>
2019-04-10 16:06:37 +0200

    User registration context tests

---
+++ apps/absence/test/absence/accounts/accounts_test.exs
@@ -0,0 +1,54 @@
+defmodule Absence.AccountsTest do
+  use Absence.DataCase, async: true
+
+  import Absence.Factory
+
+  alias Absence.Accounts
+  alias Absence.Accounts.User
+
+  describe "user_changeset/0,1" do
+    test "creates new User changeset" do
+      assert %Ecto.Changeset{data: %User{}} = Accounts.user_changeset()
+    end
+
+    test "creates User changeset from existing User" do
+      user = build(:user)
+
+      assert %Ecto.Changeset{data: ^user} = Accounts.user_changeset(user)
+    end
+  end
+
+  describe "register/1 with valid params" do
+    test "inserts User to database" do
+      assert {:ok, %User{} = user} = Accounts.register(params_for(:user))
+
+      assert Repo.get(User, user.id)
+    end
+  end
+
+  describe "register/1 with invalid params" do
+    for field <- [:first_name, :last_name, :email, :password] do
+      test "#{field} is required" do
+        assert {:error, changeset} =
+                 Accounts.register(params_for(:user, %{unquote(field) => nil}))
+
+        assert "can't be blank" in errors_on(changeset)[unquote(field)]
+      end
+    end
+
+    test "email must be unique" do
+      user = insert(:user, email: "test@example.com")
+
+      assert {:error, changeset} = Accounts.register(params_for(:user, email: user.email))
+
+      assert "has already been taken" in errors_on(changeset).email
+    end
+
+    test "password confirmation must match password" do
+      assert {:error, changeset} =
+               Accounts.register(params_for(:user, password: "a", password_confirmation: "b"))
+
+      assert "does not match confirmation" in errors_on(changeset).password_confirmation
+    end
+  end
+end
--- apps/absence/test/support/factory.ex
+++ apps/absence/test/support/factory.ex
@@ -1,4 +1,8 @@
 defmodule Absence.Factory do
+  use ExMachina.Ecto, repo: Absence.Repo
+
+  alias Absence.Accounts
+
   defdelegate build_aggregate(factory_name, attrs \\ []),
     to: Absence.Factory.AggregateFactory,
     as: :build
@@ -10,4 +14,14 @@ defmodule Absence.Factory do
   defdelegate build_event(factory_name, attrs \\ []),
     to: Absence.Factory.EventFactory,
     as: :build
+
+  def user_factory do
+    %Accounts.User{
+      first_name: "Alice",
+      last_name: "Doe",
+      email: sequence(:user_email, &"test-#{&1}@example.com"),
+      password: "P@ssw0rd",
+      password_confirmation: "P@ssw0rd"
+    }
+  end
 end
