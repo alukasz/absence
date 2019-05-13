5da7b49bb310051efb5cf9a606ec9f39b2c8e826
Łukasz Antończyk <antonczyklukasz@gmail.com>
2019-05-13 11:42:13 +0200

    Add EventSourcing.Command (#52)
    * Add EventSourcing.Command
    
    * Better naming, missing tests

---
+++ apps/event_sourcing/lib/event_sourcing/command.ex
@@ -0,0 +1,70 @@
+defmodule EventSourcing.Command do
+  defmacro __using__(_opts) do
+    quote do
+      import EventSourcing.Command
+      import Ecto.Changeset
+
+      Module.register_attribute(__MODULE__, :command, accumulate: true)
+    end
+  end
+
+  defmacro field(name, type, opts \\ []) do
+    quote do
+      Module.put_attribute(__MODULE__, :command, {unquote(name), unquote(type), unquote(opts)})
+    end
+  end
+
+  defmacro command(do: block) do
+    quote do
+      unquote(block)
+
+      @fields Enum.map(@command, &elem(&1, 0))
+      @required_fields Enum.filter(@command, fn {_, _, opts} ->
+                         Keyword.get(opts, :required, true)
+                       end)
+                       |> Enum.map(&elem(&1, 0))
+      @types Enum.map(@command, fn {name, type, _} -> {name, type} end) |> Enum.into(%{})
+      @struct_definition [uuid: nil] ++
+                           Enum.map(@command, fn {name, _, opts} ->
+                             {name, Keyword.get(opts, :default)}
+                           end)
+      @empty_data Enum.into(@struct_definition, %{})
+      @schema {@empty_data, @types}
+
+      @uuid_generator Application.get_env(:event_sourcing, :uuid_generator)
+
+      defstruct @struct_definition
+
+      def changeset do
+        cast(%{})
+      end
+
+      def build(params) do
+        params
+        |> cast()
+        |> Ecto.Changeset.put_change(:uuid, @uuid_generator.generate())
+        |> Ecto.Changeset.validate_required(@required_fields)
+        |> validate()
+        |> get_command()
+      end
+
+      def validate(changeset) do
+        changeset
+      end
+
+      defp cast(params) do
+        changeset = Ecto.Changeset.cast(@schema, params, @fields)
+        put_in(changeset.changes, Map.merge(@empty_data, changeset.changes))
+      end
+
+      defp get_command(changeset) do
+        case Ecto.Changeset.apply_action(changeset, :insert) do
+          {:ok, changes} -> {:ok, struct(__MODULE__, changes)}
+          error -> error
+        end
+      end
+
+      defoverridable validate: 1
+    end
+  end
+end
---
+++ apps/event_sourcing/test/event_sourcing/command_test.exs
@@ -0,0 +1,110 @@
+defmodule EventSourcing.CommandTest do
+  use ExUnit.Case, async: true
+
+  defmodule ExampleCommand do
+    use EventSourcing.Command
+
+    command do
+      field :foo, :integer
+      field :bar, :string
+      field :baz, :date
+    end
+  end
+
+  describe "defining command" do
+    test "struct has all defined fields" do
+      assert %{foo: _, bar: _, baz: _} = %ExampleCommand{}
+    end
+
+    test "struct has uuid field" do
+      assert %{uuid: _} = %ExampleCommand{}
+    end
+
+    test "generates changeset/0 function" do
+      assert function_exported?(ExampleCommand, :changeset, 0)
+    end
+
+    test "generates build/1 function" do
+      assert function_exported?(ExampleCommand, :build, 1)
+    end
+  end
+
+  describe "changeset/0" do
+    test "returns changeset for command" do
+      assert %Ecto.Changeset{} = ExampleCommand.changeset()
+    end
+  end
+
+  describe "build/1" do
+    defmodule FancyCommand do
+      use EventSourcing.Command
+
+      command do
+        field :default, :integer, default: 42
+        field :optional, :integer, required: false
+        field :required, :integer, required: true
+      end
+    end
+
+    test "with valid params build command" do
+      assert {:ok, %FancyCommand{default: 1, optional: 2, required: 3}} =
+               FancyCommand.build(%{default: 1, optional: 2, required: 3})
+    end
+
+    test "with invalid params return changeset" do
+      assert {:error, %Ecto.Changeset{} = changeset} = FancyCommand.build(%{})
+    end
+
+    test "sets :action on changeset" do
+      assert {:error, %Ecto.Changeset{action: :insert} = changeset} = FancyCommand.build(%{})
+    end
+
+    test "default values" do
+      assert {:error, changeset} = FancyCommand.build(%{})
+      refute Map.has_key?(errors_on(changeset), :default)
+    end
+
+    test "fields are required if not specified" do
+      assert {:error, changeset} = FancyCommand.build(%{default: nil})
+      assert "can't be blank" in errors_on(changeset).default
+    end
+
+    test "required fields" do
+      assert {:error, changeset} = FancyCommand.build(%{})
+      assert "can't be blank" in errors_on(changeset).required
+    end
+
+    test "optional fields" do
+      assert {:error, changeset} = FancyCommand.build(%{})
+      refute Map.has_key?(errors_on(changeset), :optional)
+    end
+  end
+
+  describe "validate/1" do
+    defmodule ValidateCommand do
+      use EventSourcing.Command
+
+      command do
+        field :number, :integer
+      end
+
+      def validate(changeset) do
+        validate_number(changeset, :number, equal_to: 42)
+      end
+    end
+
+    test "defines custom validation" do
+      assert {:error, changeset} = ValidateCommand.build(%{number: 12})
+
+      assert "must be equal to 42" in errors_on(changeset).number
+    end
+  end
+
+  def errors_on(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, fn {message, opts} ->
+      Enum.reduce(opts, message, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
+  end
+end
