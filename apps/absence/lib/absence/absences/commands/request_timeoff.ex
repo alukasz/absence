31fc2183deacba3324ef3cbf11c901c2b4f567a6
Łukasz Antończyk <antonczyklukasz@gmail.com>
2019-05-13 12:10:26 +0200

    Use EventSourcing.Command for RequestTimeoff command

--- apps/absence/lib/absence/absences/commands/request_timeoff.ex
+++ apps/absence/lib/absence/absences/commands/request_timeoff.ex
@@ -1,39 +1,14 @@
 defmodule Absence.Absences.Commands.RequestTimeoff do
-  defstruct [
-    :uuid,
-    :employee_uuid,
-    :start_date,
-    :end_date
-  ]
+  use EventSourcing.Command
 
-  # for now manually build/cast changeset used to create Phoenix form
-  # TODO extract common functions into EventSourcing.Command
-  # or separate "form" layer in AbsenceWeb
-  @schema %{
-    employee_uuid: Ecto.UUID,
-    start_date: :date,
-    end_date: :date
-  }
-
-  def changeset do
-    cast(%{})
-  end
-
-  def build(params) do
-    params
-    |> cast()
-    |> validate()
-    |> Ecto.Changeset.apply_action(:insert)
-    |> case do
-      {:ok, changes} -> {:ok, struct(__MODULE__, changes)}
-      error -> error
-    end
+  command do
+    field :employee_uuid, EventSourcing.UUID
+    field :start_date, :date
+    field :end_date, :date
   end
 
   defp validate(changeset) do
-    changeset
-    |> Ecto.Changeset.validate_required(Map.keys(@schema))
-    |> validate_dates_succession()
+    validate_dates_succession(changeset)
   end
 
   defp validate_dates_succession(%{valid?: false} = changeset), do: changeset
@@ -45,11 +20,5 @@ defmodule Absence.Absences.Commands.RequestTimeoff do
       :gt -> Ecto.Changeset.add_error(changeset, :end_date, "must be after start date")
       _ -> changeset
     end
-  end
-
-  defp cast(params) do
-    empty_map = Map.keys(@schema) |> Enum.reduce(%{}, fn key, acc -> Map.put(acc, key, nil) end)
-    changeset = Ecto.Changeset.cast({%{}, @schema}, params, Map.keys(@schema))
-    put_in(changeset.changes, Map.merge(empty_map, changeset.changes))
   end
 end
